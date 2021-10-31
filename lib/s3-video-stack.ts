9932c97b0117d6581c22673451fcc49b2aca6ee0
bobby ross <bobby.dev@pm.me>
2021-10-31 12:05:37 +0000

    refactor: format ts

--- lib/s3-video-stack.ts
+++ lib/s3-video-stack.ts
@@ -1,16 +1,15 @@
-import * as cdk from '@aws-cdk/core';
-import * as s3 from '@aws-cdk/aws-s3';
+import * as cdk from "@aws-cdk/core";
+import * as s3 from "@aws-cdk/aws-s3";
 
 export class VideoBucketStack extends cdk.Stack {
-    public readonly bucket: s3.IBucket;
+  public readonly bucket: s3.IBucket;
 
-    constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
-        super(scope, id, props);
+  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
 
-        this.bucket = new s3.Bucket(this, 'VideoBucket', {
-            publicReadAccess: false,
-            removalPolicy: cdk.RemovalPolicy.DESTROY
-        });
-
-    }
-}   
+    this.bucket = new s3.Bucket(this, "VideoBucket", {
+      publicReadAccess: false,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+    });
+  }
+}
